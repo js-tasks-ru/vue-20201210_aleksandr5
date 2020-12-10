a7faa65c45fec426c254a3c09dc6d70493836d3d
alex-milyaev1991 <alex.milyaev1991@gmail.com>
2020-12-10 20:18:50 +0200

    add solution for 02-components 01-CounterButtonComponent

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -1,6 +1,31 @@
 export const CounterButton = {
   // Шаблон потребуется отредактировать
-  template: '<button type="button"></button>',
+  template: '<button type="button" @click="addCount">Increment</button>',
+  props: {
+    count: {
+      type: Number,
+      default: 0
+    },
+    value: {
+      type: Number,
+      default: 0
+    }
+  },
+  data () {
+    return {
+      number: this.value || this.count
+    }
+  },
+  methods: {
+    addCount () {
+      this.number += 1
+
+      // v-model по сути это v-bind value и событие input
+      // по этому хотя мы явно и не передаем props value и не емитим собыие input это будет делаться автоматически
+      this.$emit('increment', this.number)
+      this.$emit('input', this.number)
+    }
+  }
 
   // Компонент должен иметь входной параметр
 
