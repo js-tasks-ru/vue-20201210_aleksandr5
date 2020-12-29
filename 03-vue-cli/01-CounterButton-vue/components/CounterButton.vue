ccc140802688a0639b36ec6f65ae54c70c394f5f
alex-milyaev1991 <alex.milyaev1991@gmail.com>
2020-12-29 22:55:19 +0200

    add decision for 03-vue-cli tasks

--- 03-vue-cli/01-CounterButton-vue/components/CounterButton.vue
+++ 03-vue-cli/01-CounterButton-vue/components/CounterButton.vue
@@ -1,7 +1,27 @@
-<template><div></div></template>
+<template>
+  <div>
+    <button type="button" @click="addCount">{{count}}</button>
+  </div>
+</template>
 
 <script>
-export default {};
+export default {
+  props: {
+    count: {
+      type: Number,
+      default: 0
+    }
+  },
+  methods: {
+    addCount () {
+      this.$emit('increment', this.count + 1)
+    }
+  },
+  model: {
+    prop: 'count',
+    event: 'increment'
+  }
+};
 </script>
 
 <style></style>
